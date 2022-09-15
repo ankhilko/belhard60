15ff1c379b82ff4eeaeb9f2ab82a73e26e460017
Sanr <fieldstreet.27@gmail.com>
2022-09-15 18:41:30 +0300

    Lesson 4 HT 03 corrected the first index

--- 4_3.py
+++ 4_3.py
@@ -3,6 +3,6 @@
 # для этих ключей будут браться с клавиатуры
 
 n = int(input('введите число n: '))
-dic_c = {n: {'name': input('введите имя: '), 'email': input('введите e-mail: ')} for n in range(1, n + 1)}
+dic_c = {n: {'name': input('введите имя: '), 'email': input('введите e-mail: ')} for n in range(0, n + 1)}
 
 print(dic_c)
