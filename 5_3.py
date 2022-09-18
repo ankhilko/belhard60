786df095c377aa7c1a95d4c45dd50df02c633059
Sanr <fieldstreet.27@gmail.com>
2022-09-18 03:20:33 +0300

    HT 5_3 done

--- 5_3.py
+++ 5_3.py
@@ -1,6 +1,17 @@
 # **Вывести четные числа от 2 до N по 5 в строку
 
-pass
+n = None
+
+while not n:
+    try:
+        n = int(input())
+    except ValueError:
+        print('you need to input an integer: ')
+
+for i in range(2, n + 1, 10):
+    for j in range(i, min(i + 9, n + 1), 2):
+        print(j, end=' ')
+    print()
 
 
 
