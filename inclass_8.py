cb593c27765ab31bc1507ad3c6ba17108e9ded15
sanr <fieldstreet.27@gmail.com>
2022-09-27 19:38:17 +0300

    lesson 8 part 1

--- inclass_8.py
+++ inclass_8.py
@@ -1,28 +1,77 @@
-class User(object):
+# class User(object):
+#
+#     def __init__(self, name: str, email: str, age: int):
+#         self.name = name
+#         self.email = email
+#         self.age = age
+#
+#     def birthday(self, integer=1):
+#         self.age += integer
+#
+#
+# class Manager(User):
+#     # def __init_subclass__(cls, **kwargs):
+#     #     pass
+#     def __int__(self, salary=100):
+#         super.__init__()
+#         self.salary = salary
+#
+#     def birthday(self, integer=1):
+#         self.age += integer * 3
+#
+#
+# class Boss(Manager):
+#     def __init__(self, name: str, email: str, age: int):
+#         super().__init__(name, email, age)
+# # alt + ENTER
+#
+#
+#
+#
+# vasya = Manager(
+#     "Vasya",
+#     "e@mail.ru",
+#     40
+# )
+#
+# print(vasya.age)
+# vasya.birthday()
+# print(vasya.age)
+#
+#
 
-    def __init__(self, name: str, email: str, age: int):
-        self.name = name
-        self.email = email
-        self.age = age
+class CRUDUser():
 
-    def birthday(self, integer=1):
-        self.age += integer
+    def create(self):
+        pass
+
+    def read(self):
+        pass
+
+    def update(self):
+        pass
+
+    def delete(self):
+        pass
+class CRUDCategory():
+
+    def create(self):
+        pass
+
+    def read(self):
+        pass
+
+    def update(self):
+        pass
+
+    def delete(self):
+        pass
+
+crud = CRUDUser()
+
+def foo(crud):
+    crud.get()
 
 
-class Manager(User):
-    # def __init_subclass__(cls, **kwargs):
-    #     pass
-    def __int__(self):
-        super.__init__()
 
-
-vasya = Manager(
-    "Vasya",
-    "e@mail.ru",
-    40
-)
-
-print(vasya.age)
-vasya.birthday()
-print(vasya.age)
 
